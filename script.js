da16c59141ee6b1efabb58be0ab10564ef296631
Elle <elli.g.pope@gmail.com>
2017-08-02 16:51:32 -0700

    the console log wasn't working in the function, changed the console.log in the main javascript. Now it should load right away.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var previousScroll = 0; //This updates after every scroll to afix the header only if distance is less than is used to be, or "scroll up
 
+console.log (navigator.userAgent);
+
 window.addEventListener('scroll', stickyHeader);
 
 function stickyHeader() {
 	var header =document.getElementById('header');
 	var distance = document.body.scrollTop;
-	
-	console.log (navigator.userAgent);
 	
 	if(distance <= previousScroll && distance >= 1) {
 		header.className = 'sticky-header';
